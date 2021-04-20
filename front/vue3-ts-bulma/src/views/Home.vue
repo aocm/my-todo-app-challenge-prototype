e6942ba13a15fd91e0d9fd0b4788918fede00c34
Kazuki Aoshima <aocm.dev@gmail.com>
2021-04-20 22:44:49 +0900

    レイアウトたたき１

--- front/vue3-ts-bulma/src/views/Home.vue
+++ front/vue3-ts-bulma/src/views/Home.vue
@@ -37,7 +37,8 @@
                   >
                 </li>
               </ul>
-              <p class="menu-label">More to read...</p>
+              <hr />
+              <p class="menu-label">タスクグループ</p>
               <ul class="menu-list">
                 <li><span class="tag is-white is-medium">Lorem</span></li>
                 <li><span class="tag is-white is-medium">Ipsum</span></li>
@@ -59,7 +60,7 @@
           </div>
           <div class="column is-9">
             <div class="content is-medium">
-              <h3 class="title is-3">Snippets ¯\_(ツ)_/¯</h3>
+              <h3 class="title is-3">TaskList ¯\_(ツ)_/¯</h3>
               <div class="box">
                 <h4 id="const" class="title is-3">const</h4>
                 <article class="message is-primary">
@@ -84,9 +85,7 @@
                 </article>
                 <pre><code class="language-javascript">let i = 0;</code></pre>
               </div>
-              <h3 class="title is-3">More to Come...</h3>
               <div class="box">
-                <h4 id="lorem" class="title is-4">More to come...</h4>
                 <article class="message is-primary">
                   <span class="icon has-text-primary">
                     <i class="fas fa-info-circle"></i>
@@ -96,7 +95,6 @@
                     id scaevola gloriatur instructior sit.
                   </div>
                 </article>
-                <pre><code class="language-javascript">let i = 0;</code></pre>
               </div>
             </div>
           </div>
@@ -104,140 +102,10 @@
       </div>
     </section>
     <footer class="footer">
-      <section class="section">
-        <div class="container">
-          <div class="columns is-multiline">
-            <div class="column is-one-third">
-              <article class="notification media has-background-white">
-                <figure class="media-left">
-                  <span class="icon">
-                    <i class="has-text-warning fas fa-columns fa-lg"></i>
-                  </span>
-                </figure>
-                <div class="media-content">
-                  <div class="content">
-                    <h1 class="title is-size-4">Columns</h1>
-                    <p class="is-size-5 subtitle">
-                      The power of <strong>Flexbox</strong> in a simple
-                      interface
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-            <div class="column is-one-third">
-              <article class="notification has-background-white media">
-                <figure class="media-left">
-                  <span class="icon has-text-info">
-                    <i class="fab fa-lg fa-wpforms"></i>
-                  </span>
-                </figure>
-                <div class="media-content">
-                  <div class="content">
-                    <h1 class="title is-size-4">Form</h1>
-                    <p class="is-size-5 subtitle">
-                      The indispensable <strong>form controls</strong>, designed
-                      for maximum clarity
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-            <div class="column is-one-third">
-              <article class="notification has-background-white media">
-                <figure class="media-left">
-                  <span class="icon has-text-danger">
-                    <i class="fas fa-lg fa-cubes"></i>
-                  </span>
-                </figure>
-                <div class="media-content">
-                  <div class="content">
-                    <h1 class="title is-size-4">Components</h1>
-                    <p class="is-size-5 subtitle">
-                      Advanced multi-part components with lots of possibilities
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-            <div class="column is-one-third">
-              <article class="notification has-background-white media">
-                <figure class="media-left">
-                  <span class="icon has-text-grey">
-                    <i class="fas fa-lg fa-cogs"></i>
-                  </span>
-                </figure>
-                <div class="media-content">
-                  <div class="content">
-                    <h1 class="title is-size-4">Modifiers</h1>
-                    <p class="is-size-5 subtitle">
-                      An <strong>easy-to-read</strong> naming system designed
-                      for humans
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-            <div class="column is-one-third">
-              <article class="notification has-background-white media">
-                <figure class="media-left">
-                  <span class="icon has-text-primary">
-                    <i class="fas fa-lg fa-warehouse"></i>
-                  </span>
-                </figure>
-                <div class="media-content">
-                  <div class="content">
-                    <h1 class="title is-size-4">Layout</h1>
-                    <p class="is-size-5 subtitle">
-                      Design the <strong>structure</strong> of your webpage with
-                      these CSS classes
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-            <div class="column is-one-third">
-              <article class="notification has-background-white media">
-                <figure class="media-left">
-                  <span class="icon has-text-danger">
-                    <i class="fas fa-lg fa-cube"></i>
-                  </span>
-                </figure>
-                <div class="media-content">
-                  <div class="content">
-                    <h1 class="title is-size-4">Elements</h1>
-                    <p class="is-size-5 subtitle">
-                      Essential interface elements that only require a
-                      <strong>single CSS class</strong>
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-          </div>
-        </div>
-      </section>
-      <hr />
       <div class="columns is-mobile is-centered">
         <div class="field is-grouped is-grouped-multiline">
           <div class="control">
-            <div class="tags has-addons">
-              <a
-                class="tag is-link"
-                href="https://github.com/BulmaTemplates/bulma-templates"
-                >Bulma Templates</a
-              >
-              <span class="tag is-info">MIT license</span>
-            </div>
-          </div>
-          <div class="control">
-            <div class="tags has-addons">
-              <span class="tag is-dark">based on a pen</span>
-              <span class="tag has-addons is-warning"
-                ><a href="https://codepen.io/melanieseltzer/pen/odOXWM"
-                  ><i class="fab fa-lg fa-codepen"></i></a
-              ></span>
-            </div>
+            <span class="tag is-dark">footer</span>
           </div>
         </div>
       </div>
