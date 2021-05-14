b5a6932549c41c48ee60be54b3e27539e6a29417
Kazuki Aoshima <aocm.dev@gmail.com>
2021-05-14 22:49:36 +0900

    start.spring.io でSpringBatch作成

---
+++ batch/src/main/java/com/aocm/sample/spbatch/SpbatchApplication.java
@@ -0,0 +1,13 @@
+package com.aocm.sample.spbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbatchApplication.class, args);
+	}
+
+}
---
+++ batch/src/test/java/com/aocm/sample/spbatch/SpbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aocm.sample.spbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
