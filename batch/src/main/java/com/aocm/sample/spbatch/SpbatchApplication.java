5bd4159aed71f0d3ae4a1e1016aaac234e946b2d
Kazuki Aoshima <aocm.dev@gmail.com>
2021-05-15 20:20:16 +0900

    SpringBatchを終了コードを出力するように修正
    参考：https://www.ois-yokohama.co.jp/oisblog2018/archives/6498

--- batch/src/main/java/com/aocm/sample/spbatch/SpbatchApplication.java
+++ batch/src/main/java/com/aocm/sample/spbatch/SpbatchApplication.java
@@ -1,13 +1,23 @@
 package com.aocm.sample.spbatch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@Slf4j
 public class SpbatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpbatchApplication.class, args);
+		log.info("start... {}", String.join(", ", args));
+		ApplicationContext context = SpringApplication.run(SpbatchApplication.class, args);
+		int exitCode = SpringApplication.exit(context);
+		log.info("exit... {}", exitCode);
+		System.exit(exitCode);
 	}
 
 }
